b0ebea74e8e079fa2d114955f742be14dc8c09bc
sourabh Jain <sourabhjain0412@gmail.com>
2022-11-08 14:24:28 +0530

    This is my example change

---
+++ SpringCF/src/main/java/sourabh/SpringCF/MainApp.java
@@ -0,0 +1,15 @@
+package sourabh.SpringCF;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		JavaCollection jc = (JavaCollection) context.getBean("javaCollection");
+		jc.getAddressList();
+		jc.getAddressSet();
+		jc.getAddressMap();
+		jc.getAddressProp();
+	}
+}
